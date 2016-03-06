8fcf54424ba6bec93c04e9e4d52885f4d0526d15
notepi <panpengok@126.com>
2016-03-05 18:49:18 -0800

    更新常用命令

--- nginx/objs/ngx_auto_config.h
+++ nginx/objs/ngx_auto_config.h
@@ -1,4 +1,4 @@
-#define NGX_CONFIGURE " --with-debug --prefix=/home/linux/share/servers --add-module=/home/linux/share/module/ngx_http_test_module"
+#define NGX_CONFIGURE " --with-debug --prefix=/home/linux/share/servers"
 
 #ifndef NGX_DEBUG
 #define NGX_DEBUG  1
